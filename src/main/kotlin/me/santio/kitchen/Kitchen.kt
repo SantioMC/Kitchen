a820a38bcf80baa2c3b6ff8978922f56cff59ce2
SantioMC <business@santio.me>
2023-10-06 19:29:38 -0400

    fix: tech

--- src/main/kotlin/me/santio/kitchen/Kitchen.kt
+++ src/main/kotlin/me/santio/kitchen/Kitchen.kt
@@ -13,6 +13,7 @@ import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.koin.core.KoinApplication
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.context.GlobalContext.startKoin
 import org.koin.dsl.module
 
@@ -37,11 +38,11 @@ class Kitchen: JavaPlugin(), KoinComponent {
         server.pluginManager.registerEvents(InventoryListener, this)
         server.pluginManager.registerEvents(ComponentListener, this)
 
-        koinApp.koin.get<ComponentRegistry>().apply { import() }
+        get<ComponentRegistry>().apply { import() }
     }
 
     override fun onDisable() {
-        koinApp.koin.get<ComponentRegistry>().apply { export() }
+        get<ComponentRegistry>().apply { export() }
     }
 
     companion object {
